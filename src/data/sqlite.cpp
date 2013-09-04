3f61702fd233274996f8e960eb621283549409a0
Ciprian <ciliesj@gmail.com>
2013-09-03 22:38:12 -0700

    Buggered sqlite functions, for some reason the blarging compiler is blind and doesn't see the sqlite header.

---
+++ src/data/sqlite.cpp
@@ -0,0 +1,96 @@
+/* Copyright Ciprian Ilies */
+
+#pragma once
+
+#include "sqlite.hpp"
+
+#include <sqlite3.h>
+
+namespace mdata
+{
+
+namespace sqlite
+{
+
+Database::Database()
+{
+	db = nullptr;
+}
+
+Database::~Database()
+{
+	this->close();
+}
+
+void Database::open(std::string filepath)
+{
+	//This really shouldn't be called if db already exists.
+	if (db)
+	{
+		sqlite3_close(db);
+		delete db;
+	}
+
+	std::string error_message = "";
+	int rc;
+
+	rc = sqlite3_open(database.c_str(), &db);
+
+	if (rc)
+	{
+		fprintf(stderr, "\n[mdata::sqlite::Database] ERROR: Can't open database: %s.\n", sqlite3_errmsg(db));
+		exit(0);
+	}
+	else
+	{
+		std::cout << "\n[mdata::sqlite::Database] Opened database successfully.\n";
+	}
+}
+
+void Database::close()
+{
+	if (db)
+	{
+		sqlite3_close(db);
+		delete db;
+		db = nullptr;
+	}
+}
+
+static int Database::SQLiteCallback(void *not_used, int argc, char **argv, char **col_name)
+{
+	int i;
+	for (i=0; i<argc; i++)
+	{
+		printf("%s = %s\n", col_name[i], argv[i] ? argv[i] : "NULL");
+	}
+	std::cout << "\n";
+	return 0;
+}
+
+/*
+ * Creates a table, if it doesn't exist.
+ */
+void createTable(std::string table_name, std::string column_args)
+{
+	std::string sql = "CREATE TABLE IF NOTE EXISTS " + table_name + "(" + column_args + ");";
+
+	char* error_message = nullptr;
+
+	int rc = sqlite3_exec(db, sql, SQLiteCallback, 0, &error_message);
+	if (rc != SQLITE_OK)
+	{
+		fprintf(stderr, "\n[mdata::sqlite::Database] ERROR: SQL error: %s\n", error_message);
+		sqlite3_free(error_message);
+	}
+	else
+	{
+		std::cout << "\n[mdata::sqlite::Database] Table created successfully\n";
+	}
+}
+
+};
+
+} //namespace sqlite
+
+} //namespace mdata
--- src/data/sqlite.hpp
+++ src/data/sqlite.hpp
@@ -23,84 +23,22 @@ namespace sqlite
 class Database
 {
 private:
-	sqlite3* db;
+	sqlite3 *db;
 public:
-	Database()
-	{
-		db = nullptr;
-	}
+	Database();
 
-	~Database()
-	{
-		this->close();
-	}
+	~Database();
 
-	void open(std::string filepath)
-	{
-		//This really shouldn't be called if db already exists.
-		if (db)
-		{
-			sqlite3_close(db);
-			delete db;
-		}
+	void open(std::string filepath);
 
-		std::string error_message = "";
-		int rc;
+	void close();
 
-		rc = sqlite3_open(database.c_str(), &db);
-
-		if (rc)
-		{
-			fprintf(stderr, "\n[mdata::sqlite::Database] ERROR: Can't open database: %s.\n", sqlite3_errmsg(db));
-			exit(0);
-		}
-		else
-		{
-			std::cout << "\n[mdata::sqlite::Database] Opened database successfully.\n";
-		}
-	}
-
-	void close()
-	{
-		if (db)
-		{
-			sqlite3_close(db);
-			delete db;
-			db = nullptr;
-		}
-	}
-
-	static int SQLiteCallback(void *not_used, int argc, char **argv, char **col_name)
-	{
-		int i;
-		for (i=0; i<argc; i++)
-		{
-			printf("%s = %s\n", col_name[i], argv[i] ? argv[i] : "NULL");
-		}
-		std::cout << "\n";
-		return 0;
-	}
+	static int SQLiteCallback(void *not_used, int argc, char **argv, char **col_name);
 
 	/*
 	 * Creates a table, if it doesn't exist.
 	 */
-	void createTable(std::string table_name, std::string column_args)
-	{
-		std::string sql = "CREATE TABLE IF NOTE EXISTS " + table_name + "(" + column_args + ");";
-
-		char* error_message = nullptr;
-
-		int rc = sqlite3_exec(db, sql, SQLiteCallback, 0, &error_message);
-		if (rc != SQLITE_OK)
-		{
-			fprintf(stderr, "\n[mdata::sqlite::Database] ERROR: SQL error: %s\n", error_message);
-			sqlite3_free(error_message);
-		}
-		else
-		{
-			std::cout << "\n[mdata::sqlite::Database] Table created successfully\n";
-		}
-	}
+	void createTable(std::string table_name, std::string column_args);
 
 	/*
 	 * Returns false if table does not exist, true if table exists:

9c8229cd490db4d8f2b76ddd587f5de4548de54c
Ciprian <ciliesj@gmail.com>
2013-09-05 09:52:49 -0700

    Oops forgot the license and header.

---
+++ src/gengine.hpp
@@ -0,0 +1,16 @@
+/* Copyright Ciprian Ilies */
+
+#include "config/config.hpp"
+#include "data/data.hpp"
+#include "data/sqlite.hpp" //TODO: Make this an optional include.
+#include "error/error.hpp"
+#include "file/file.hpp"
+#include "graphic/graphic.hpp"
+#include "graphic/2d/2d.hpp"
+#include "graphic/window/window.hpp"
+#include "interface/interface.hpp"
+#include "math/math.hpp"
+#include "net/client.hpp"
+#include "net/server.hpp"
+#include "rand/rand.hpp"
+#include "string/string.hpp"

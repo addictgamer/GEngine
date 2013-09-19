7802f307b2d4e8f1c7c1528ee49d7f560ef66455
Ciprian <ciliesj@gmail.com>
2013-09-19 10:29:11 -0700

    Added set window function.

--- src/graphic/3d/3d.cpp
+++ src/graphic/3d/3d.cpp
@@ -2,6 +2,8 @@
 
 #include "3d.hpp"
 
+#include "../window/window.hpp"
+
 namespace GEngine
 {
 
@@ -13,15 +15,22 @@ namespace d3d
 
 D3D::D3D()
 {
+	window = nullptr;
 }
 
 D3D::~D3D()
 {
+	window = nullptr;
 }
 
 bool D3D::setup()
 {
 	return true; //Success.
+}
+
+void D3D::setWindow(Window &_window)
+{
+	window = &_window;
 }
 
 void D3D::update()
--- src/graphic/3d/3d.hpp
+++ src/graphic/3d/3d.hpp
@@ -10,17 +10,24 @@ namespace GEngine
 namespace mgfx
 {
 
+class Window; //Forward declaration.
+
 namespace d3d
 {
 
 class D3D
 {
 public:
+	Window *window; //The window being used with this. //NOTE: Please do NOT change this at runtime. Create a new d2d for every window.
+
 	D3D();
 	~D3D();
 
 	//Sets up the 3D manager thingy.
 	bool setup();
+
+	//Be sure to call setWindow() before using D3D!
+	void setWindow(Window &_window);
 
 	//Updates the 3D manager thingy.
 	void update();

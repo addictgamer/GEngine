bb0429b05c07d121cfe1f5327cd8707fd6253654
Ciprian <ciliesj@gmail.com>
2013-10-15 11:12:40 -0700

    Drawing only the default GUI context.

--- src/interface/interface.cpp
+++ src/interface/interface.cpp
@@ -194,7 +194,8 @@ void Interface::update()
 
 	//onMouseButtonUp //Use this to check if the frame window's button was pressed.
 
-	CEGUI::System::getSingleton().renderAllGUIContexts(); //Render all of CEGUI's stuffs.
+	//CEGUI::System::getSingleton().renderAllGUIContexts(); //Render all of CEGUI's stuffs. //TODO: Make this work properly later.
+	CEGUI::System::getSingleton().getDefaultGUIContext().draw();
 	CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().draw(); //Force draw it because it doesn't seem to want to work otherwise.
 }
 

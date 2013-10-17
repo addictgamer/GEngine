a8aacc12f790c06686f4cdda5d462ba43d09e181
Ciprian <ciliesj@gmail.com>
2013-10-16 18:44:11 -0700

    Even more stuff.

--- src/interface/interface.cpp
+++ src/interface/interface.cpp
@@ -141,7 +141,7 @@ bool Interface::loadFont(std::string filepath)
 void Interface::update()
 {
 	std::vector<mgfx::d2d::D2D* >::iterator iter;
-	for (iter = windows.begin(); iter != windows.end(); iter++)
+	for (iter = windows.begin(); iter != windows.end(); ++iter)
 	//for (int i = 0; i < windows.size(); ++i)
 	{
 		mgfx::d2d::D2D *d2d = *iter; //First point to this so I don't have to type crazy things every time.
